63f91c4090f85edc430a5ec0c13b6e42751e759c
Amir <amirrfz2003@gmail.com>
2025-06-04 21:01:07 +0330

    add first video

---
+++ 1/math.js
@@ -0,0 +1,11 @@
+// const add = (a,b) => a + b;
+// const sub = (a,b) => a - b;
+// const mul = (a,b) => a * b;
+// const dev = (a,b) => a / b;
+
+// module.exports = {add, sub, dev, mul}
+
+exports.add = (a,b) => a + b;
+exports.sub = (a,b) => a - b;
+exports.mul = (a,b) => a * b;
+exports.dev = (a,b) => a / b;
---
+++ 1/server.js
@@ -0,0 +1,22 @@
+// console.log("hello word")
+
+// console.log(global)
+
+const os = require('os')
+const path = require('path')
+// const math = require('./math')
+const {sub} = require('./math')
+
+// console.log(math.add(1,3))
+console.log(sub(1,3))
+
+// console.log(os.type())
+// console.log(os.version())
+
+// console.log(__dirname)
+// console.log(__filename)
+
+// console.log(path.dirname(__dirname))
+// console.log(path.basename(__dirname))
+
+// console.log(path.parse(__dirname))
