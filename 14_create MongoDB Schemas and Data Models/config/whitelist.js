4dc3357052a8eec186a342e56543859fea011fcf
Amir <amirrfz2003@gmail.com>
2025-07-18 22:15:37 +0330

    start 14

---
+++ 14_create MongoDB Schemas and Data Models/config/whitelist.js
@@ -0,0 +1,7 @@
+const whitelist = ['https://www.google.com',
+                    'http://www.yoursite.com',
+                    'http://127.0.0.1:5500',
+                    'http://localhost:8080'
+                ];
+
+module.exports = whitelist;
