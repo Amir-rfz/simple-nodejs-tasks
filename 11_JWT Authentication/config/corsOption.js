4a81d5a79b27d59f56aa99814da2e74a312bb43c
Amir <amirrfz2003@gmail.com>
2025-07-18 16:55:20 +0330

    complete video 11

--- 11_JWT Authentication/config/corsOption.js
+++ 11_JWT Authentication/config/corsOption.js
@@ -1,8 +1,6 @@
 //cross-origin resource sharing (CORS) middleware
-const whitelist = ['https://www.google.com',
-                    'http://www.yoursite.com',
-                    'http://127.0.0.1:5500',
-                    'http://localhost:8080'];
+const whitelist = require('./whitelist'); //import the whitelist from config/whitelist.js
+
 const corsOptions = {
     origin: (origin, callback) => {
         if(whitelist.indexOf(origin) !== -1 || !origin){
--- 11_JWT Authentication/server.js
+++ 11_JWT Authentication/server.js
@@ -5,11 +5,16 @@ const {logger} = require('./middleware/logEvents');
 const cors = require('cors');
 const corsOptions = require('./config/corsOption')
 const errorHandler = require('./middleware/errorHandler');
+const verifyJWT = require('./middleware/verifyJWT');
+const cookieParser = require('cookie-parser');
+const credentials = require('./middleware/credentials');
 const { el } = require('date-fns/locale');
 const PORT = process.env.PORT || 8080;
 
 //custom middleware logger
 app.use(logger);//logger defines in the logEvents.js file
+
+app.use(credentials); //enable credentials for CORS
 
 app.use(cors(corsOptions)); //enable CORS for all routes
 
@@ -18,11 +23,18 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json());
 
+//middleware to parse cookies
+app.use(cookieParser());
+
 app.use('/', express.static(path.join(__dirname, '/public')));
 
 app.use('/', require('./routes/root')); //mounting the root router
 app.use('/register', require('./routes/register'));
 app.use('/auth', require('./routes/auth'));
+app.use('/refresh', require('./routes/refresh')); //route for handling refresh tokens
+app.use('/logout', require('./routes/logout')); //route for handling logout
+
+app.use(verifyJWT); //apply JWT verification middleware to all routes below this line
 app.use('/employees', require('./routes/api/employees'))
 
 // if the user tries to access any other route, we will send a 404 page
