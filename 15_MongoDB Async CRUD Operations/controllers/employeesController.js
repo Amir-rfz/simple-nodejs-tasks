c9c1149d03ae50c16a171759524db1aa97f70d80
Amir <amirrfz2003@gmail.com>
2025-07-19 13:10:01 +0330

    complete 15

--- 15_MongoDB Async CRUD Operations/controllers/employeesController.js
+++ 15_MongoDB Async CRUD Operations/controllers/employeesController.js
@@ -1,55 +1,60 @@
-const data = {
-    employees : require('../model/employees.json'),
-    setEmployees : function (data) {
-        this.employees = data;
+const Employee = require('../model/Employee');
+
+const getAllEmployees = async (req, res) => {
+    try {
+        const employees = await Employee.find();
+        res.json(employees);
+    } catch (error) {
+        res.status(500).json({ 'message': error.message });
     }
 }
 
-const getAllEmployees = (req,res)=>{
-        res.json(data.employees);
+const creatNewEmployee = async (req, res) => {
+    try {
+        const newEmployee = new Employee({
+            firstname: req.body.firstname,
+            lastname: req.body.lastname
+        });
+        const savedEmployee = await newEmployee.save();
+        res.status(201).json(savedEmployee);
+    } catch (error) {
+        res.status(500).json({ 'message': error.message });
     }
-
-const creatNewEmployee = (req,res)=>{
-    const newEmployee ={
-        id: data.employees[data.employees.length -1].id +1 |1,
-        firstname: req.body.firstname,
-        lastname: req.body.lastname
-    }
-
-    if (!newEmployee.firstname || !newEmployee.lastname) {
-        return res.status(400).json({ 'message': 'First and last names are required.' });
-    }
-
-    data.setEmployees([...data.employees, newEmployee]);
-    res.status(201).json(data.employees);
 }
 
-const updateEmployee = (req,res)=>{
-    const employee = data.employees.find(emp => emp.id === parseInt(req.body.id));
+const updateEmployee = async (req, res) => {
+    try {
+        const employee = await Employee.findOne({ id: req.body.id }).exec();
+        if (!employee) {
+            return res.status(204).json({ "message": `Employee ID ${req.body.id} not found` });
+        }
+    } catch (error) {
+        return res.status(500).json({ 'message': error.message });
+    }
+    if (req.body?.firstname) employee.firstname = req.body.firstname;
+    if (req.body?.lastname) employee.lastname = req.body.lastname;
+    const result = await employee.save();
+    res.json(result);
+
+}
+
+const deleteEmolpoyee = async (req,res)=>{
+    if (!req.body?.id) {
+        return res.status(400).json({ "message": "Employee ID required." });
+    }
+    const employee = await Employee.findOne({ id: req.body.id }).exec();
     if (!employee) {
-        return res.status(400).json({ "message": `Employee ID ${req.body.id} not found` });
+        return res.status(204).json({ "message": `Employee ID ${req.body.id} not found` });
     }
-    if (req.body.firstname) employee.firstname = req.body.firstname;
-    if (req.body.lastname) employee.lastname = req.body.lastname;
-    const filteredArray = data.employees.filter(emp => emp.id !== parseInt(req.body.id));
-    const unsortedArray = [...filteredArray, employee];
-    data.setEmployees(unsortedArray.sort((a, b) => a.id > b.id ? 1 : a.id < b.id ? -1 : 0));
-    res.json(data.employees);
-
+    const result = await Employee.deleteOne({ id: req.body.id });
+    res.json(result);
 }
 
-const deleteEmolpoyee = (req,res)=>{
-    const employee = data.employees.find(emp => emp.id === parseInt(req.body.id));
-    if (!employee) {
-        return res.status(400).json({ "message": `Employee ID ${req.body.id} not found` });
+const getEmployee = async (req,res)=>{
+    if (!req.params?.id) {
+        return res.status(400).json({ "message": "Employee ID required." });
     }
-    const filteredArray = data.employees.filter(emp => emp.id !== parseInt(req.body.id));
-    data.setEmployees([...filteredArray]);
-    res.json(data.employees);
-}
-
-const getEmployee = (req,res)=>{
-    const employee = data.employees.find(emp => emp.id === parseInt(req.params.id));
+    const employee = await Employee.findOne({ id: req.params.id }).exec();
     if (!employee) {
         return res.status(400).json({ "message": `Employee ID ${req.params.id} not found` });
     }
--- 15_MongoDB Async CRUD Operations/controllers/logoutController.js
+++ 15_MongoDB Async CRUD Operations/controllers/logoutController.js
@@ -1,11 +1,4 @@
-const usersDB = {
-    users: require('../model/users.json'),
-    setUsers: function (data) { this.users = data }
-}
-
-const fsPromises = require('fs').promises;
-const { max } = require('date-fns');
-const path = require('path');
+const User = require('../model/User');
 
 const handleLogout = async (req, res) => {
     //on client, also delete the accessToken
@@ -16,21 +9,18 @@ const handleLogout = async (req, res) => {
     const refreshToken = cookies.jwt;
 
     //if refreshToken in db, delete it
-    const foundUser = usersDB.users.find(person => person.refreshToken === refreshToken);
+    const foundUser = await User.findOne({ refreshToken }).exec();
     if (!foundUser) {
-        res.clearCookie('jwt', { httpOnly: true, secure: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 }); // secure true - only server on https
+        res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 }); // secure true - only server on https
         return res.sendStatus(204); // No Content
     }
 
-    // delete refreshToken in db
-    const otherUsers = usersDB.users.filter(person => person.refreshToken !== refreshToken);
-    const currentUser = { ...foundUser, refreshToken: '' };
-    usersDB.setUsers([...otherUsers, currentUser]);
-    await fsPromises.writeFile(
-        path.join(__dirname, '..', 'model', 'users.json'),
-        JSON.stringify(usersDB.users)
-    );
-    res.clearCookie('jwt', { httpOnly: true , secure: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000}); // secure true - only server on https
+    // delete refreshToken in db\
+    foundUser.refreshToken = '';
+    const result = await foundUser.save();
+    console.log(result);
+    
+    res.clearCookie('jwt', { httpOnly: true , sameSite: 'None', maxAge: 24 * 60 * 60 * 1000}); // secure true - only server on https
     res.sendStatus(204); // No Content
 }
 
